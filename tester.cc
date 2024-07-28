6917b666be68362396bf5d604e5ddfcffe275348
Omar Saleh <omar.saleh@dor-e.com>
2024-07-28 14:59:53 +0200

    - Multithreading tests. - Fix cmake typo.

--- tester.cc
+++ tester.cc
@@ -1,9 +1,12 @@
 #include <gtest/gtest.h>
+#include <thread>
+#include <future>
 #include "HashTable.hpp"
 
+using hash_table = celonis::ConcurrentHashTable<std::string, std::string>;
 
 TEST(SingleThread, SingleInsert) {
-    celonis::ConcurrentHashTable<std::string, std::string> test(1);
+    hash_table test(1);
     test.put("Hello", "World");
     auto res = test.get("Hello");
     EXPECT_TRUE(res.has_value());
@@ -11,7 +14,7 @@ TEST(SingleThread, SingleInsert) {
 }
 
 TEST(SingleThread, SameKeyMultiInsert) {
-    celonis::ConcurrentHashTable<std::string, std::string> test(1);
+    hash_table test(1);
     test.put("Hello", "World");
     test.put("Hello", "Concurrent");
     auto res = test.get("Hello");
@@ -20,7 +23,7 @@ TEST(SingleThread, SameKeyMultiInsert) {
 }
 
 TEST(SingleThread, DeletedKey) {
-    celonis::ConcurrentHashTable<std::string, std::string> test(1);
+    hash_table test(1);
     test.put("Hello", "World");
     test.erase("Hello");
     auto res = test.get("Hello");
@@ -28,7 +31,7 @@ TEST(SingleThread, DeletedKey) {
 }
 
 TEST(SingleThread, DeletedThenReinserted) {
-    celonis::ConcurrentHashTable<std::string, std::string> test(1);
+    hash_table test(1);
     test.put("Hello", "World");
     test.erase("Hello");
     test.put("Hello", "HashTable");
@@ -37,3 +40,47 @@ TEST(SingleThread, DeletedThenReinserted) {
     EXPECT_EQ(res.value(), "HashTable");
 }
 
+void multiThreadFunction(hash_table& table, std::string own_key, std::string other_key) {
+    table.put(own_key, "ConcurrencyCheck");
+    auto res = table.get(own_key);
+    EXPECT_TRUE(res.has_value());
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    res = table.get(other_key);
+    EXPECT_TRUE(res.has_value());
+    EXPECT_EQ(res.value(), "ConcurrencyCheck");
+};
+
+void insertKeyWithThread(hash_table& table, std::string own_key) {
+    table.put(own_key, "ConcurrencyCheck");
+    EXPECT_TRUE(table.get(own_key));
+}
+
+void mutiThreadDeleteFromOtherThread(hash_table& table, std::string other_key) {
+    table.erase(other_key);
+};
+
+TEST(MultiThread, ThreadsCanReadFromEachOther) {
+    hash_table test(1);
+    auto t1 = std::async(std::launch::async, multiThreadFunction, std::ref(test), "T1", "T2");
+    auto t2 = std::async(std::launch::async, multiThreadFunction, std::ref(test), "T2", "T1");
+
+    t1.wait();
+    t2.wait();
+
+    auto res = test.get("T1");
+    EXPECT_TRUE(res.has_value());
+    res = test.get("T2");
+    EXPECT_TRUE(res.has_value());
+}
+
+TEST(MultiThread, ThreadsCanDeleteFromEachOther) {
+    hash_table test(1);
+    auto t1 = std::async(std::launch::async, insertKeyWithThread, std::ref(test), "T1");
+    auto t2 = std::async(std::launch::async, mutiThreadDeleteFromOtherThread, std::ref(test), "T1");
+
+    t1.wait();
+    t2.wait();
+
+    auto res = test.get("T1");
+    EXPECT_FALSE(res.has_value());
+}
